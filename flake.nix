67e30f728a5a123debdfa83ff81e36b8a48ea877
Stanisław Pitucha <stan.pitucha@envato.com>
2022-02-10 13:48:01 +1100

    Cleanup

--- flake.nix
+++ flake.nix
@@ -1,39 +1,40 @@
 {
   description = "cf-vault";
 
-  outputs = { self, nixpkgs }: let
-      # List of systems supported by home-manager binary
-      supportedSystems = nixpkgs.lib.platforms.unix;
-
-      # Function to generate a set based on supported systems
-      forAllSystems = f:
-        nixpkgs.lib.genAttrs supportedSystems (system: f system);
-
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-    in rec {
-    defaultPackage = forAllSystems (system: nixpkgsFor.${system}.buildGoModule rec {
-      pname = "cf-vault";
-      version = "0.0.11";
-
-      src = nixpkgsFor.${system}.fetchFromGitHub {
-        owner = "jacobbednarz";
-        repo = "cf-vault";
-        rev = "${version}";
-        sha256 = "sha256-Imd9qeT4xg5ujVPLHSSqoteSPl9t97q3Oc4C/vzHphg=";
-      };
-
-      proxyVendor = true;
-
-      vendorSha256 = null;
-    });
-
-    apps = forAllSystems (system: {
-      cf-vault = {
-        type = "app";
-        program = "${defaultPackage.${system}}/bin/cf-vault";
-      };
-    });
-
-    defaultApp = forAllSystems (system: apps.${system}.cf-vault);
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in rec {
+          defaultPackage = pkgs.buildGoModule rec {
+            pname = "cf-vault";
+            version = "0.0.11";
+
+            src = pkgs.fetchFromGitHub {
+              owner = "jacobbednarz";
+              repo = "cf-vault";
+              rev = "${version}";
+              sha256 = "sha256-Imd9qeT4xg5ujVPLHSSqoteSPl9t97q3Oc4C/vzHphg=";
+            };
+
+            proxyVendor = true;
+
+            vendorSha256 = "sha256-SEC/qhvBmUpdXvAHWKylyiBl/MxThFAqy+0DcTDb9N4=";
+          };
+
+          apps = {
+            cf-vault = {
+              type = "app";
+              program = "${defaultPackage.${system}}/bin/cf-vault";
+            };
+          };
+
+          defaultApp = apps.${system}.cf-vault;
+        }
+      );
 }
